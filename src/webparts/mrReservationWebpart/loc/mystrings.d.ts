2650d0413f0276ecf1e7424af63a25ff527fb546
afahru <m.fahrurrozi@outlook.com>
2023-02-24 17:32:47 +0700

    Initial

---
+++ src/webparts/mrReservationWebpart/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IMrReservationWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'MrReservationWebpartWebPartStrings' {
+  const strings: IMrReservationWebpartWebPartStrings;
+  export = strings;
+}
